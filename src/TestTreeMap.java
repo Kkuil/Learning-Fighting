c3f80acc02fb42e7d5d4a6c72aa86a76e0c405ec
Kkuil <3024067144@qq.com>
2023-07-28 17:59:31 +0800

    TreeMap concept

---
+++ src/TestTreeMap.java
@@ -0,0 +1,10 @@
+package src;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class TestTreeMap {
+    public static void main(String[] args) {
+        TreeMap<String, Object> map = new TreeMap<>();
+    }
+}
